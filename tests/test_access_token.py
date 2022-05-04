1467d94b7a10e719a84ac7dfdfb15c6f10e472b6
payls <payalsonawane@gmail.com>
2022-05-04 14:14:13 +0530

    Update test_access_token.py

--- tests/test_access_token.py
+++ tests/test_access_token.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 from fake_data import inputs
 from helper import resolve_app_path
 resolve_app_path()
-import lambda_function
+import lambda_function # noqa
 
 
 # user inputs
